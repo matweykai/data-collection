7f1221b4fea3ce18a4cec11ff545b842484222aa
Matwey Ivanov <matveykai@list.ru>
2025-04-19 12:26:18 +0300

    Update pipeline.py with YOLO labeling asset

--- dagster-project/pipeline.py
+++ dagster-project/pipeline.py
@@ -2,6 +2,7 @@ import dagster as dg
 
 from assets.collected_data import collected_data_from_dogtime, collected_data_from_dogs_in_depth, merged_parsed_data
 from assets.cleaned_data import cleaned_data
+from assets.labeled_data import labeled_data
 
 
 defs = dg.Definitions(
@@ -9,6 +10,7 @@ defs = dg.Definitions(
         collected_data_from_dogtime,
         collected_data_from_dogs_in_depth,
         merged_parsed_data,
-        cleaned_data
+        cleaned_data,
+        labeled_data,
     ],
 )
